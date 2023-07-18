d405aabb60097fed4e8dceb1212c52c2f1e34e2a
quangdao2601 <tranquangdao2601@gmail.com>
2023-07-18 12:48:40 +0700

    html

--- quangdao.php
+++ quangdao.php
@@ -1,4 +1,12 @@
-<?php 
-
-
-echo "al";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Quang Đạo</p>
+    <a href="">Tôi là Quang Đạo</a>
+</body>
+</html>
